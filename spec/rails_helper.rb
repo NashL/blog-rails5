12f219bed78a2e4f6846442ed00308fd75ec9069
Oscar Luza <onluza@gmail.com>
2018-08-04 03:18:10 -0500

    Added exception_handler handler to applicatonController

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,14 +36,9 @@ end
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-RSpec.configuration do |config|
-  # [...]
-  config.include RequestSpecHelper, type: :request
-  # [...]
-end
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include RequestSpecHelper, type: :request
   config.include FactoryBot::Syntax::Methods
   # config.include RequestSpecHelper, type: :request
 
